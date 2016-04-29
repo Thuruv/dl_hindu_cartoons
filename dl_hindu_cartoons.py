a36ff4af446bc8ec3bf72977ded7cefcae526678
thuruvan <wasp52dex@gmail.com>
2016-04-30 03:54:27 +0530

    Added app

---
+++ dl_hindu_cartoons.py
@@ -0,0 +1,33 @@
+import mechanize
+import re
+from bs4 import BeautifulSoup
+
+
+br = mechanize.Browser()
+#br.set_all_readonly(False)
+br.set_handle_robots(False)
+br.set_handle_refresh(False)
+br.addheaders =  [('User-agent', 'Firefox')]
+url = 'http://www.thehindu.com/opinion/cartoon/'
+response = br.open(url)
+html =  response.read()
+
+dl_urls = dict()
+
+soup = BeautifulSoup(html)
+temp = []
+for link in br.links(url_regex = 'http://www.thehindu.com/opinion/cartoon/+[?](.*)'):
+    temp.append(str(link).split("'")[3])
+
+
+for page_url in list(set(temp)):
+    response = br.open(page_url)
+    html =  response.read()
+    soup = BeautifulSoup(html)
+    for i in soup.find_all('div',{'class' : 'section-teaser'}):
+        url,date  = re.sub('d.jpg', 'f.jpg', i.a.img['src']), ((i.h2.text).split('-')[-1]).strip()
+        print url,date
+        dl_urls[date] = re.sub('d.jpg', 'f.jpg', url)        
+        image_response = br.open_novisit(url)
+        with open(date + '.jpg', 'wb') as f:
+            f.write(image_response.read())
