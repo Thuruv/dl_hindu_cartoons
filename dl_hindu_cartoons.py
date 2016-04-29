0cc4ab0ba1cec10dc856059d510565481b86572e
thuruvan <wasp52dex@gmail.com>
2016-04-30 04:03:44 +0530

    .

--- dl_hindu_cartoons.py
+++ dl_hindu_cartoons.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import mechanize
 import re
 from bs4 import BeautifulSoup
 
 
 br = mechanize.Browser()
-#br.set_all_readonly(False)
+
+#   setup the browser for action
+br.set_all_readonly(False)
 br.set_handle_robots(False)
 br.set_handle_refresh(False)
 br.addheaders =  [('User-agent', 'Firefox')]
+
+#   capture the base page
 url = 'http://www.thehindu.com/opinion/cartoon/'
 response = br.open(url)
 html =  response.read()
@@ -16,10 +23,11 @@ dl_urls = dict()
 
 soup = BeautifulSoup(html)
 temp = []
+#   get the PTO pages
 for link in br.links(url_regex = 'http://www.thehindu.com/opinion/cartoon/+[?](.*)'):
     temp.append(str(link).split("'")[3])
 
-
+#   workaround the each image section
 for page_url in list(set(temp)):
     response = br.open(page_url)
     html =  response.read()
